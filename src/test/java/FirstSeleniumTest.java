50a642bd8418ea8337e84a8f30f14354ddc5c89a
And1377 <karalu590@gmail.com>
2022-10-29 01:23:25 +0300

    Adding web driver/ working with buttons/ finding element on page

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -9,6 +9,7 @@ import java.time.Duration;
 
 public class FirstSeleniumTest {
     private final By ACCEPT_COOKIES_BTN = By.id("onetrust-accept-btn-handler");
+    private final By LATVIA_COUNTRY_BTN = By.xpath("//a[text()='Latvia']");
     @Test
     public void openHomePageCheck() {
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
@@ -19,5 +20,7 @@ public class FirstSeleniumTest {
         WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
         browser.findElement(ACCEPT_COOKIES_BTN).click();
+        wait.until(ExpectedConditions.elementToBeClickable(LATVIA_COUNTRY_BTN));
+        browser.findElement(LATVIA_COUNTRY_BTN).click();
     }
 }
