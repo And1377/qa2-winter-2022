78fbd6757d84b13a388612d278d8c743d459623d
And1377 <karalu590@gmail.com>
2022-11-17 00:07:59 +0200

    xpath/homework/100% complete/fixed path length/fin

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -12,10 +12,10 @@ import java.util.List;
 
 public class FirstSeleniumTest {
     private final By ACCEPT_COOKIES_BTN = By.id("onetrust-accept-btn-handler");
-    private final By LATVIA_COUNTRY_BTN = By.xpath("//a[text()='Latvia']");
+    private final By LATVIA_COUNTRY_BTN = By.xpath(".//div[@class = 'tl-container']/div");
     private final By TOP_LOCATION_LINK = By.xpath(".//div[@class = 'tl-box']/a");
     //first page homework---------
-    private final By LOGO_BTN_MAIN = By.xpath("//div[@class='logo-cell']");
+    private final By LOGO_BTN_MAIN = By.xpath(".//div[@class='logo-cell']");
     private final By CURRENCY_SELECTOR = By.id("currentCurrency");
     private final By LOGIN_BTN = By.className("trigger-login-modal");
     private final By HOME_CAPTION = By.xpath("//div[@class ='home-caption']");
@@ -30,21 +30,19 @@ public class FirstSeleniumTest {
     private final By CAR_SELECTION_SLIDER_BTN_RIGHT = By.xpath(".//div[@class = 'car-top-filter-control next']");
     private final By SHOWN_CARS_ON_PAGE = By.xpath(".//span[@class = 'showing-cars']");
     private final By SORT_BY_PRICE_BTN_RIGHT = By.xpath(".//div[@class = 'dc-ui dropdown inline-block']");
-
     private final By CAR_DESCRIPTION_SECTION_BAG = By.xpath(".//ul[contains(@class, 'dc-list-md text-14')]/li");
     private final By SPECIAL_OFFERS_LEFT_COLUMN = By.xpath(".//div[@class='flt-type'][1]");
     private final By CAR_SPECIFICATIONS_PRICE = By.xpath(".//span[@class = 'min-price']");
     private final By CAR_SPECIFICATIONS_FUEL_TYPE = By.xpath(".//div[@id='filter-fuel-type']/label");
     private final By NUMBER_OF_SEATS = By.xpath(".//div[@id='filter-number-of-seats']/label");
-
     private final By RENTAL_CONDITIONS = By.xpath(".//a[contains(@class, 'search-index')]");
     private final By RENTAL_VIEW_BTN = By.xpath(".//a[contains(@class, 'btn')]");
     private final By PARTIAL_PREPAYMENT = By.xpath(".//span[contains(@class, 'dc-ui')]");
     private final By CAR_SCORE = By.xpath(".//div[@class = 'supplier-rating']");
 //loading page*----------
-    private final By LOADING_ANIMATION_CIRCLES = By.xpath(".//*div[contains(@class, 'lds-ellipsis v2')]");
-    private final By WE_ARE_CURRENTLY_SEARCHING_TXT = By.xpath("//*[@id='main-wrapper-body']//div[contains(@class, 'mb-8 text-24 text-bold text-center')]");
-    private final By FOR_THE_BEST_TXT = By.xpath("//*[@id='main-wrapper-body']//div[contains(@class, 'mb-24 text-center')]");
+    private final By LOADING_ANIMATION_CIRCLES = By.xpath(".//div[@class = 'lds-ellipsis v2']");
+    private final By WE_ARE_CURRENTLY_SEARCHING_TXT = By.xpath(".//div[@class = 'mb-8 text-24 text-bold text-center']");
+    private final By FOR_THE_BEST_TXT = By.xpath(".//div[@class = 'mb-24 text-center']");
 
     @Test
     public void openHomePageCheck() {
