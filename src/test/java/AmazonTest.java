bd7e06ac6693f8f5e4b20eecd78699025c7a351d
And1377 <karalu590@gmail.com>
2023-02-21 18:18:53 +0200

    Amazon homework finished and working without Thread sleep, registration and SMS.

--- src/test/java/AmazonTest.java
+++ src/test/java/AmazonTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -19,7 +20,6 @@ public class AmazonTest {
     private final By AMAZON_MAIN_MENU_ITEM = By.xpath(".//div[@id = 'nav-xshop']/a");
     private final By AMAZON_LEFT_MENU_ITEM = By.xpath(".//div[@role = 'treeitem']/a");
     private final By AMAZON_BOOK_PAGE_THUMBNAILS = By.xpath(".//div[@class = 'p13n-sc-uncoverable-faceout']/a[1]");
-    private final By AMAZON_BOOK_PAGE_THUMBNAIL_STARS = By.xpath(".//span[@class = 'a-icon-alt']");
     private final By AMAZON_ALL_COMMENTS_BTN = By.xpath(".//a[@data-hook= 'see-all-reviews-link-foot']");
     private final By AMAZON_COMMENT_ITEM = By.xpath(".//div[@class = 'a-section celwidget']");
     private final By AMAZON_COMMENT_NEXT_PAGE_BTN = By.xpath(".//li[@class = 'a-last']/a");
@@ -27,7 +27,9 @@ public class AmazonTest {
     private final By AMAZON_TOTAL_REVIEWS_COUNT = By.xpath(".//div[@data-hook='cr-filter-info-review-rating-count']");
 
     private WebDriver browser;
-    private WebDriverWait wait; //https://www.seleniumeasy.com/selenium-tutorials/webdriver-wait-examples
+    private WebDriverWait wait;
+
+    //https://www.seleniumeasy.com/selenium-tutorials/webdriver-wait-examples
 //https://youtu.be/uWnfiI9CL1g?list=PL29imBtAdLy-9H5wHMT0BRF4RziIQuAEr&t=3959 adding list to another list
     //menuItems.addAll(!browser.findElements()) - adding list to another list
 //    while (!browser.findElements(NEXT_BTN).isEmpty()) {
@@ -37,26 +39,28 @@ public class AmazonTest {
 //    }
 
     @Test
-    public void amazonTest() throws InterruptedException {
-        //Test Data
+    public void amazonTest() {
         String menuItemToSelect = "Best Sellers";
         String menuItemLeft = "Books";
         int bookPositionNumberInTop = 3;
         int commentCountAllPages = 0;
+
 
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
         browser = new ChromeDriver();
         browser.manage().window().maximize();
         browser.get("https://www.amazon.de/");
 
-        wait = new WebDriverWait(browser, Duration.ofSeconds(10));
+        wait = new WebDriverWait(browser, Duration.ofSeconds(2));
 
         closeAllMessages();
         openMenuItem(menuItemToSelect, AMAZON_MAIN_MENU_ITEM);
         openMenuItem(menuItemLeft, AMAZON_LEFT_MENU_ITEM);
-        String itemStarScore = browser.findElements(AMAZON_BEST_SELLER_SECTION_STAR_AMOUNT).get(bookPositionNumberInTop - 1).getAttribute("textContent").substring(0, 3);
+        String itemStarScore = browser.findElements(AMAZON_BEST_SELLER_SECTION_STAR_AMOUNT).
+                get(bookPositionNumberInTop - 1).getAttribute("textContent").substring(0, 3);
         openSpecificItemByNumber(bookPositionNumberInTop, AMAZON_BOOK_PAGE_THUMBNAILS);
-        String starsInDescription = browser.findElement(AMAZON_BEST_SELLER_SECTION_STAR_AMOUNT).getAttribute("textContent").substring(0, 3);
+        String starsInDescription = browser.findElement(AMAZON_BEST_SELLER_SECTION_STAR_AMOUNT).
+                getAttribute("textContent").substring(0, 3);
         Assertions.assertEquals(itemStarScore, starsInDescription, "Star amount doesn't matches!");
 
         //https://youtu.be/wg0w5l-Snrw?t=4342 -- wait and Thread sleep
@@ -65,29 +69,25 @@ public class AmazonTest {
         click(AMAZON_ALL_COMMENTS_BTN);
         getReviewCount();
         while (isPaginationActive()) {
-            commentCountAllPages += browser.findElements(AMAZON_COMMENT_ITEM).size();
+            List<WebElement> existingElements = browser.findElements(AMAZON_COMMENT_ITEM);
+            commentCountAllPages += existingElements.size();
             //https://www.w3docs.com/snippets/java/stale-element-reference-element-is-not-attached-to-the-page-document.html
-            System.out.println(commentCountAllPages);
-            // Thread.sleep(500);
+            //
+            //System.out.println(commentCountAllPages);
             try {
                 click(AMAZON_COMMENT_NEXT_PAGE_BTN);
             } catch (WebDriverException e) {
                 if (e.getClass().equals(StaleElementReferenceException.class)) {
                     click(AMAZON_COMMENT_NEXT_PAGE_BTN);
-                } else if (e.getClass().equals(TimeoutException.class)) {
-                    commentCountAllPages += browser.findElements(AMAZON_COMMENT_ITEM).size();
-                    System.out.println(commentCountAllPages + "kek");
                 } else {
-                    System.out.println("default");
+                    System.out.println("Exception was something other than Stale element");
                 }
-
-
             }
-            Thread.sleep(2000);
+            wait.until(ExpectedConditions.invisibilityOf(existingElements.get(0)));
         }
-        // commentCountAllPages += browser.findElements(AMAZON_COMMENT_ITEM).size();
-        System.out.println("Actual amount of comments is: " + commentCountAllPages);
-        System.out.println("Amount of comments " + getReviewCount());
+        commentCountAllPages += browser.findElements(AMAZON_COMMENT_ITEM).size();
+        System.out.println("Amount of counted comments is: " + commentCountAllPages);
+        System.out.println("Amount of comments in description: " + getReviewCount());
         Assertions.assertEquals(getReviewCount(), commentCountAllPages, "Error- values not equal");
     }
 
@@ -95,8 +95,6 @@ public class AmazonTest {
     //https://youtu.be/uWnfiI9CL1g?list=PL29imBtAdLy-9H5wHMT0BRF4RziIQuAEr&t=3746
     private void openMenuItem(String itemName, By locator) {
         List<WebElement> menuItems = browser.findElements(locator);
-//        WebElement we - variable where we will store each list variable
-//        menuItems - list that we are checking
         for (WebElement we : menuItems) {
             if (we.getText().equals(itemName)) {
                 we.click();
@@ -105,27 +103,13 @@ public class AmazonTest {
         }
     }
 
-//    private void starCounter(int itemPositionInBs, By locator) {
-//        List<WebElement> menuItems = browser.findElements(locator);
-//        for (WebElement we : menuItems) {
-//
-//        }
-//    }
-
     //https://youtu.be/fm_7RFDKN0k?list=PL29imBtAdLy-9H5wHMT0BRF4RziIQuAEr&t=4199 extract value from string
     //
 
     private void openSpecificItemByNumber(int bookPositionNumberInTop, By locator) {
         List<WebElement> productItems = browser.findElements(locator);
         productItems.get(bookPositionNumberInTop - 1).click();
-//        System.out.println(productItems.size());
     }
-
-//    private void rememberSpecificItemStars(int itemPositionInTop, By locator) {
-//        List<WebElement> productItems = browser.findElements(locator);
-////        productItems.get(itemPositionInTop - 1).getText();
-//        System.out.println(productItems.get(itemPositionInTop - 1).getText());
-//    }
 
     private void closeAllMessages() {
         wait.until(ExpectedConditions.elementToBeClickable(AMAZON_ACCEPT_COOKIES_BTN)); //https://habr.com/ru/post/443754/
@@ -146,11 +130,6 @@ public class AmazonTest {
         } catch (TimeoutException e) {
             return false;
         }
-
-    }
-
-    private List<WebElement> findElements(By locator) {
-        return browser.findElements(locator);
     }
 
     private WebElement findElement(By locator) {
@@ -162,17 +141,8 @@ public class AmazonTest {
         return Integer.parseInt(StringUtils.substringBetween(text, "ratings, ", " with"));
     }
 
-//    private void select(By locator, String value) {
-//        WebElement we = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
-//        Select select = new Select(we);
-//        select.selectByValue(value);
-//    }
-//    private void waitForElementsCountToBe(By locator, int count) {
-//        wait.until(ExpectedConditions.numberOfElementsToBe(locator, count));
-//    }
-
     @AfterEach
     public void closeBrowser() {
-        // browser.close();
+        browser.close();
     }
 }
